ec240b948f5a444a606dca750fc2db71286ecaff
Richard Pitts <richardepitts@gmail.com>
2019-07-03 14:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,11 +37,9 @@ function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] == findFn()) {
       return array[i]
-    } else {
-      return null
     }
 }
-
+return null
 }
 
 
